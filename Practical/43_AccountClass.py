646f01fc794cae162c9a147add29860c54f5efa1
CSGS <148139834+sreasgop@users.noreply.github.com>
2025-05-07 21:00:08 +0530

    Program no 43 completed and output added

--- Practical/43_AccountClass.py
+++ Practical/43_AccountClass.py
@@ -15,34 +15,65 @@ class Account:
         Account.count += 1
 
     def get_balance(self):
-        return self.balance
+        print(f"Account Balance of {self.account_holders_name}: {self.balance}\n")
 
     def deposit(self, amount):
-        if amount > 0:
-            self.balance += amount
-        print(f"Account no: {self.account_number} successfully deposited with amount Rs.{amount}\nTotal Balance: Rs.{self.balance}\n")
+        if amount < 0:
+            print("Invalid Amount!")
+        self.balance += amount
+        print(f"Account no: {self.account_number} credited with amount Rs.{amount}\nCurrent Balance: Rs.{self.balance}.\n")
 
     def withdraw(self, amount):
         if self.balance < amount:
             print("Insufficient balance in account.")
         else:
             self.balance -= amount
-    
+        print(f"Account no: {self.account_number} debited with amount Rs.{amount}\nCurrent Balance: Rs.{self.balance}.\n")
+
     @classmethod
     def get_count(cls):
-        return cls.count
+        return f"Total No. of accounts created: {cls.count}"
 
     def __str__(self):
         return f"Account Holder's Name: {self.account_holders_name}\nAccount Number: {self.account_number}\nBalance: {self.balance}\n"
+
 
 
 a1 = Account("Chandra Sreas Gop", 2451, 100000)
 a2 = Account("Soutrika Das", 2454, 99800)
 a3 = Account("Sanjib Kumar Sah", 2492, 70000)
 
-a1.deposit(16000)
-
 print(a1)
 print(a2)
 print(a3)
-print("No of Accounts:", Account.get_count())
+
+a1.deposit(16000)
+a1.withdraw(590)
+a1.get_balance()
+
+print(Account.get_count())
+
+
+
+# OUTPUT:
+# Account Holder's Name: Chandra Sreas Gop
+# Account Number: 2451
+# Balance: 100000
+#
+# Account Holder's Name: Soutrika Das
+# Account Number: 2454
+# Balance: 99800
+#
+# Account Holder's Name: Sanjib Kumar Sah
+# Account Number: 2492
+# Balance: 70000
+#
+# Account no: 2451 credited with amount Rs.16000
+# Current Balance: Rs.116000.
+#
+# Account no: 2451 debited with amount Rs.590
+# Current Balance: Rs.115410.
+#
+# Account Balance of Chandra Sreas Gop: 115410
+#
+# Total No. of accounts created: 3
